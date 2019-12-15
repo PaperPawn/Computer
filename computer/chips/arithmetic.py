072dc9fcfec50489221183c764e680cc8815150e
msn <mads.stormo.nilsson@gmail.com>
2019-12-15 12:37:21 +0100

    Full Adder

--- computer/chips/arithmetic.py
+++ computer/chips/arithmetic.py
@@ -1,7 +1,14 @@
-from computer.chips.logic_gates import XOR, AND
+from computer.chips.logic_gates import XOR, AND, OR
 
 
 def half_adder(a, b):
     sum_ab = XOR(a, b)
     carry = AND(a, b)
     return sum_ab, carry
+
+
+def full_adder(a, b, c):
+    sum_bc, carry_1 = half_adder(b, c)
+    sum_abc, carry_2 = half_adder(a, sum_bc)
+    carry = OR(carry_1, carry_2)
+    return sum_abc, carry
--- computer/chips/tests/test_arithmetic.py
+++ computer/chips/tests/test_arithmetic.py
@@ -1,6 +1,6 @@
 import pytest
 
-from computer.chips.arithmetic import half_adder
+from computer.chips.arithmetic import half_adder, full_adder
 
 
 class TestHalfAdder:
@@ -12,3 +12,18 @@ class TestHalfAdder:
     @pytest.mark.parametrize('a, b, expected_sum, expected_carry', truth_table)
     def test_half_adder(self, a, b, expected_sum, expected_carry):
         assert half_adder(a, b) == (expected_sum, expected_carry)
+
+
+class TestFullAdder:
+    truth_table = [(0, 0, 0, 0, 0),
+                   (0, 0, 1, 1, 0),
+                   (0, 1, 0, 1, 0),
+                   (0, 1, 1, 0, 1),
+                   (1, 0, 0, 1, 0),
+                   (1, 0, 1, 0, 1),
+                   (1, 1, 0, 0, 1),
+                   (1, 1, 1, 1, 1)]
+
+    @pytest.mark.parametrize('a, b, c, expected_sum, expected_carry', truth_table)
+    def test_full_adder(self, a, b, c, expected_sum, expected_carry):
+        assert full_adder(a, b, c) == (expected_sum, expected_carry)

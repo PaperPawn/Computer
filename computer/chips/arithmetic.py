07f2f98d82f26af809e86f254694a9b43be75aa6
msn <mads.stormo.nilsson@gmail.com>
2019-12-15 13:09:00 +0100

    INC16

--- computer/chips/arithmetic.py
+++ computer/chips/arithmetic.py
@@ -1,4 +1,4 @@
-from computer.chips.logic_gates import XOR, AND, OR
+from computer.chips.logic_gates import XOR, AND, OR, NOT
 
 
 def half_adder(a, b):
@@ -12,3 +12,21 @@ def full_adder(a, b, c):
     sum_abc, carry_2 = half_adder(a, sum_bc)
     carry = OR(carry_1, carry_2)
     return sum_abc, carry
+
+
+def ADD16(a, b):
+    out = [0]*16
+
+    out[-1], carry = half_adder(a[-1], b[-1])
+    for i in range(2, 17):
+        out[-i], carry = full_adder(a[-i], b[-i], carry)
+    return out
+
+
+def INC16(a):
+    out = [0]*16
+    out[-1] = NOT(a[-1])
+    carry = a[-1]
+    for i in range(2, 17):
+        out[-i], carry = half_adder(a[-i], carry)
+    return out
--- computer/chips/tests/test_arithmetic.py
+++ computer/chips/tests/test_arithmetic.py
@@ -1,6 +1,6 @@
 import pytest
 
-from computer.chips.arithmetic import half_adder, full_adder
+from computer.chips.arithmetic import half_adder, full_adder, ADD16, INC16
 
 
 class TestHalfAdder:
@@ -27,3 +27,47 @@ class TestFullAdder:
     @pytest.mark.parametrize('a, b, c, expected_sum, expected_carry', truth_table)
     def test_full_adder(self, a, b, c, expected_sum, expected_carry):
         assert full_adder(a, b, c) == (expected_sum, expected_carry)
+
+
+class TestAdd16:
+    truth_table = [([0]*16, [0]*16, [0]*16),
+                   ([0]*16, [1]*16, [1]*16),
+                   ([1]*16, [0]*16, [1]*16),
+                   ([1]*16, [1]*16, [1]*15 + [0]),
+                   ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
+
+                   ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]),
+
+                   ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]),
+
+                   ([0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1],
+                    [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1])]
+
+    @pytest.mark.parametrize('a, b, expected', truth_table)
+    def test_add16(self, a, b, expected):
+        assert ADD16(a, b) == expected
+
+
+class TestInc16:
+    truth_table = [([0]*16, [0]*15 + [1]),
+                   ([0]*15 + [1], [0]*14 + [1, 0]),
+                   ([0] + [1]*15, [1] + [0]*15),
+                   ([1]*16, [0]*16),
+
+                   ([0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1]),
+
+                   ([0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1],
+                    [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0])
+                   ]
+
+    @pytest.mark.parametrize('a, expected', truth_table)
+    def test_add16(self, a, expected):
+        assert INC16(a) == expected
